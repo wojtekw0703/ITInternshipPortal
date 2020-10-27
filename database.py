50e13556bd814e87bbbd35f3dc1d4670dc1efa26
Wojciech Wydmuch <wojciechwydmuch73@gmail.com>
2020-10-27 23:20:59 +0100

    Fix bugs

--- database.py
+++ database.py
@@ -2,21 +2,20 @@ from scraping import list_of_id,list_of_company,list_of_locations, list_of_title
 import mysql.connector
 from mysql.connector import Error
 
-def insertVariblesIntoTable(id, job_title, company_name,location, url):
+def insertVariblesIntoTable(id, job_title, company_name, location, url_address):
     try:
         connection = mysql.connector.connect(host='localhost',
-                                             database='internship_database',
-                                             table ='internship',
-                                             user='root@',
-                                             password='')
+                                             database='******',
+                                             user='****',
+                                             password='****')
         cursor = connection.cursor()
-        mySql_insert_query = """INSERT INTO internship (id, job_title, company_name, location, url) 
-                                VALUES (%s, %s, %s, %s) """
+        mySql_insert_query = """INSERT INTO internships (id, job_title, company_name, location, url_address) 
+                                VALUES (%s, %s, %s, %s, %s) """
 
-        recordTuple = (id, job_title, company_name,location,url)
+        recordTuple = (id, job_title, company_name, location, url_address)
         cursor.execute(mySql_insert_query, recordTuple)
         connection.commit()
-        print("Record inserted successfully into Laptop table")
+        print("Record inserted successfully into internship table")
 
     except mysql.connector.Error as error:
         print("Failed to insert into MySQL table {}".format(error))
@@ -27,5 +26,8 @@ def insertVariblesIntoTable(id, job_title, company_name,location, url):
             connection.close()
             print("MySQL connection is closed")
 
-for i in list_of_id:
+#insertVariblesIntoTable()
+
+for i in range(0,len(list_of_id)):
     insertVariblesIntoTable(list_of_id[i],list_of_titles[i],list_of_company[i],list_of_locations[i],list_of_urls[i])
+
