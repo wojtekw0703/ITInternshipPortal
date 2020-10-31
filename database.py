9287b91967fe8d713158aed27148134453390b02
Wojciech Wydmuch <wojciechwydmuch73@gmail.com>
2020-10-31 22:39:12 +0100

    Insert values from another webiste to database

--- database.py
+++ database.py
@@ -1,13 +1,14 @@
-from scraping import list_of_id,list_of_company,list_of_locations, list_of_titles, list_of_urls
+from indeed_scraping import list_of_id,list_of_company,list_of_locations, list_of_titles, list_of_urls
+from wayup_scraping import list_of_job_titles,href_list
 import mysql.connector
 from mysql.connector import Error
 
 def insertVariblesIntoTable(id, job_title, company_name, location, url_address):
     try:
         connection = mysql.connector.connect(host='localhost',
-                                             database='******',
-                                             user='****',
-                                             password='****')
+                                             database='internship_database',
+                                             user='root@',
+                                             password='')
         cursor = connection.cursor()
         mySql_insert_query = """INSERT INTO internships (id, job_title, company_name, location, url_address) 
                                 VALUES (%s, %s, %s, %s, %s) """
@@ -26,8 +27,38 @@ def insertVariblesIntoTable(id, job_title, company_name, location, url_address):
             connection.close()
             print("MySQL connection is closed")
 
-#insertVariblesIntoTable()
-
 for i in range(0,len(list_of_id)):
     insertVariblesIntoTable(list_of_id[i],list_of_titles[i],list_of_company[i],list_of_locations[i],list_of_urls[i])
 
+from indeed_scraping import list_of_id,list_of_company,list_of_locations, list_of_titles, list_of_urls
+import mysql.connector
+from mysql.connector import Error
+
+def insertVariblesIntoTable(id, job_title, company_name, location, url_address):
+    try:
+        connection = mysql.connector.connect(host='localhost',
+                                             database='***,
+                                             user='***',
+                                             password='***')
+        cursor = connection.cursor()
+        mySql_insert_query = """INSERT INTO internships (id, job_title, company_name, location, url_address) 
+                                VALUES (%s, %s, %s, %s, %s) """
+
+        recordTuple = (id, job_title, company_name, location, url_address)
+        cursor.execute(mySql_insert_query, recordTuple)
+        connection.commit()
+        print("Record inserted successfully into internship table")
+
+    except mysql.connector.Error as error:
+        print("Failed to insert into MySQL table {}".format(error))
+
+    finally:
+        if (connection.is_connected()):
+            cursor.close()
+            connection.close()
+            print("MySQL connection is closed")
+
+for i in range(0,len(list_of_id)):
+    insertVariblesIntoTable(list_of_id[i],list_of_titles[i],list_of_company[i],list_of_locations[i],list_of_urls[i])
+for i in range(0,len(list_of_job_titles)):
+    insertVariblesIntoTable(i, list_of_job_titles[i], "None","-", href_list[i])
